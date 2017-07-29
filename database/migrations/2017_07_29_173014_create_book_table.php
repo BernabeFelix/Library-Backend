7bd3d9a49a0fe3c07ae21787a86fd11e63080dad
Bernabe <bernabe@goout.mx>
2017-07-29 12:56:41 -0500

    Added migrations for book and category

---
+++ database/migrations/2017_07_29_173014_create_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('author');
+            $table->integer('category_id');
+            $table->date('published_at');
+            $table->string('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
